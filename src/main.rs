5822293952bbc1c37aecf4096d14be1f3b59904b
jburnett <jburnett@altamodatech.com>
2020-02-03 17:55:55 -0500

    Comments on compiler error

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,12 @@ fn main() {
     let s1 = String::from("Hello!");
     let s2 = s1;
 
-//    println!("\tString value (heap) s1 = {}", s1);
+    // Uncommenting the following statement results in compiler error
+    // println!("\tString value (heap) s1 = {}", s1);
     println!("\tString value (heap) s2 = {}", s2);
 
+    // The compiler error is
+    //  error[E0382]: borrow of moved value: `s1`
+    // which is caused by s2 taking ownership of the string in the line
+    //  let s2 = s1;
 }
